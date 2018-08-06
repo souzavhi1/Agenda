19fd1c2102d8322fe93e33690fe4e899213565d9
souzavhi1 <souzavhi1@gmail.com>
2018-08-05 22:37:30 -0300

    Excluindo ok Alterando aluno

--- app/src/main/java/com/example/victor/agenda/ListaAlunoActivity.java
+++ app/src/main/java/com/example/victor/agenda/ListaAlunoActivity.java
@@ -3,10 +3,15 @@ package com.example.victor.agenda;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.victor.agenda.dao.AlunoDAO;
 import com.example.victor.agenda.modelo.Aluno;
@@ -15,10 +20,32 @@ import java.util.List;
 
 public class ListaAlunoActivity extends AppCompatActivity {
 
+    private ListView listaAlunos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_lista_aluno );
+
+        listaAlunos = (ListView) findViewById( R.id.lista_alunos );
+
+        listaAlunos.setOnItemClickListener( new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition( position );
+
+
+            }
+        } );
+
+        listaAlunos.setOnItemLongClickListener( new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                
+                return false;
+            }
+        } );
+
 
         Button novoAluno = (Button) findViewById( R.id.novo_aluno );
         novoAluno.setOnClickListener( new View.OnClickListener() {
@@ -29,6 +56,7 @@ public class ListaAlunoActivity extends AppCompatActivity {
             }
         } );
 
+        registerForContextMenu( listaAlunos );
     }
 
     private void carregaLista() {
@@ -37,7 +65,7 @@ public class ListaAlunoActivity extends AppCompatActivity {
         dao.close();
 
 //        String[] alunos ={"Daniel","Ronaldo","Jeferson","Felipe","Daniel","Ronaldo","Jeferson","Felipe","Daniel","Ronaldo","Jeferson","Felipe","Daniel","Ronaldo","Jeferson","Felipe"};
-        ListView listaAlunos = (ListView) findViewById( R.id.lista_alunos );
+
         ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>( this,android.R.layout.simple_list_item_1,alunos );
         listaAlunos.setAdapter( adapter );
     }
@@ -46,6 +74,30 @@ public class ListaAlunoActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         carregaLista();
-        
+
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        MenuItem deletar = menu.add( "Deletar" );
+        deletar.setOnMenuItemClickListener( new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition( info.position );
+
+                AlunoDAO dao = new AlunoDAO( ListaAlunoActivity.this );
+
+                dao.deleta(aluno);
+                dao.close();
+                Toast.makeText( ListaAlunoActivity.this, "Aluno " + aluno.getNome() + " deletado!", Toast.LENGTH_SHORT).show();
+                carregaLista();
+
+                return false;
+            }
+        } );
+
+
+    }
+
 }
--- app/src/main/java/com/example/victor/agenda/dao/AlunoDAO.java
+++ app/src/main/java/com/example/victor/agenda/dao/AlunoDAO.java
@@ -72,4 +72,11 @@ public class AlunoDAO extends SQLiteOpenHelper {
 
         return alunos;
     }
+
+    public void deleta(Aluno aluno) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        String[] params = {aluno.getId().toString()};
+        db.delete( "Alunos","id = ?",params );
+    }
 }
